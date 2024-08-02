53768c56fc2233dcc5eff41167b71811b2ba56a3
Rohit Mondal <rohitmondal@gmail.com>
2024-08-02 18:30:12 +0530

    error fixed

--- src/components/AddTodo.jsx
+++ src/components/AddTodo.jsx
@@ -1,8 +1,8 @@
 import { useState } from "react";
 
 function AddTodo({ onNewItem }) {
-  const [todoName, setTodoName] = useState();
-  const [todoDate, setTodoDate] = useState();
+  const [todoName, setTodoName] = useState("");
+  const [todoDate, setTodoDate] = useState("");
   const handleNameChange = (event) => {
     setTodoName(event.target.value);
   };
